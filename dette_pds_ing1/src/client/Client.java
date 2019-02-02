7e078bb64f79393d8ec39abbb44d927c6f0cf1bd
Dan <s.danith@gmail.com>
2019-02-02 12:57:50 +0100

    Connexion client + serveur + json

---
+++ dette_pds_ing1/src/client/Client.java
@@ -0,0 +1,35 @@
+package client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Client
+{
+
+ public static void main (String[]args) throws IOException, JSONException{
+   Socket socket= new Socket ("localhost", 5555);
+   OutputStreamWriter writer = new OutputStreamWriter(socket.getOutputStream(), "UTF-8");
+   BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream(),"UTF-8"));
+   
+   JSONObject jsonObject = new JSONObject();
+   jsonObject.put("message", "The Hate U Give Little Enfants F Everybody");
+   
+   
+   writer.write(jsonObject.toString()+"\n");
+   writer.flush();
+   
+   String line = reader.readLine();
+   jsonObject = new JSONObject(line);
+   
+   System.out.println("Received from Server:\n " + jsonObject.toString(2));
+  
+   socket.close();
+   
+ }
+}
